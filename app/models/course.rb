a8588b2f15b28ecff720f4e671c5c547f006c76b
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2022-01-10 14:43:44 +0000

    model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,10 @@
 class Course < ApplicationRecord
+  belongs_to :restaurant
+  has_many :books
+
+  attachment :image
+
+  validates :name, presence: true
+  validates :total_payment, presence: true
+  validates :rest_id, presence: true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :books, dependent: :destroy
+  has_many :courses, dependent: :destroy
+
+  validates :name, presence: true
 end
