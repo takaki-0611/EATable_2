6520a5e08886210051d069440f49f791647b5a2b
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2022-01-12 14:58:25 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,32 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'contacts/index'
-    get 'contacts/confirm'
-    get 'contacts/done'
+  #user側
+  devise_for :users, :controllers => {:sessions => 'public/sessions',
+    :registrations => 'public/registrations',
+    :passwords => 'public/passwords'
+  }
+  root 'public/homes#top'
+  get '/about' => 'public/homes#about'
+  resources :users, only:[:show, :edit, :update] do
+    collection do
+      get 'quit'
+      patch 'out'
+    end
   end
-  namespace :public do
-    get 'restaurants/index'
-    get 'restaurants/show'
-    get 'restaurants/edit'
-    get 'restaurants/update'
+  resources :restaurants, only:[:index, :show]
+  resources :courses, only:[:index, :show] do
+    resources :books, only:[:new, :create, :index, :show] do
+      collection do
+        post 'confirm'
+        get 'thanx'
+      end
+    end
   end
-  namespace :public do
-    get 'courses/edit'
-    get 'courses/create'
-    get 'courses/update'
-    get 'courses/destroy'
-  end
-  namespace :public do
-    get 'books/new'
-    get 'books/confirm'
-    get 'books/create'
-    get 'books/thanx'
-    get 'books/index'
-    get 'books/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-    get 'users/quit'
-    get 'users/out'
+  resources :contacts, only:[:index] do
+    collection do
+      post 'confirm'
+      get 'done'
+    end
   end
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
