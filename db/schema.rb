85ce9cb2e210165fe38ce976b44b9a1a440ec77c
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2022-01-24 14:12:20 +0000

    edit画面の追加

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2022_01_09_130112) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.string "name"
     t.string "first_name"
     t.string "last_name"
